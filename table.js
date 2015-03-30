898c9364549a5d6152c839dbd8b0f8187ab3b30a
Moazzam Khan <moazzamkhan29@gmail.com>
2015-03-30 10:31:22 +0800

    MVP First demo

---
+++ table.js
@@ -0,0 +1,165 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<title>Tembusu College Orientation</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
+		<script src="js/jquery.min.js"></script>
+		<script src="js/jquery.dropotron.min.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/skel-layers.min.js"></script>
+		<script src="js/init.js"></script>
+		<noscript>
+			<link rel="stylesheet" href="css/skel.css" />
+			<link rel="stylesheet" href="css/style.css" />
+			<link rel="stylesheet" href="css/style-desktop.css" />
+		</noscript>
+		<!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="css/ie/v9.css" /><![endif]-->
+	</head>
+	<body class="homepage">
+
+		<!-- Header -->
+			<div id="header-wrapper">
+				<div class="container">
+
+					<!-- Header -->
+						<header id="header">
+							<div class="inner">
+							
+								<!-- Logo -->
+									<h1><a href="index.html" id="logo">Tembusu College Orientation</a></h1>
+								
+								<!-- Nav -->
+									<nav id="nav">
+										<ul>
+											<li class="current_page_item">
+											<a href="index.html">Home</a></li>
+											<li>
+												<a href="orientationGroups.html">Orientation Groups</a>
+											<li><a href="timeline.html">Timeline</a></li>
+											<li><a href="aboutus.html">About Us</a></li>
+											<li><a href="gallery.html">Gallery	</a></li>
+											<!-- <li><a href="login.html">Login</a></li> -->
+
+										</ul>
+									</nav>
+							
+							</div>
+						</header>
+
+					<!-- Banner -->
+						<div id="banner">
+							<h2><strong>Welcome</strong> to Tembusu College	
+							<h3>Your home of possibilities</h3>
+							<br />
+							<img id = "mainPic" src="a.jpg" alt = "this shows a picture of the college logo" width = "60%" height = "10%">
+						</div>
+
+				</div>
+			</div>
+		
+		<!-- Main Wrapper -->
+			<div id="main-wrapper">
+				<div class="wrapper style1">
+					<div class="inner">
+				
+						<!-- Feature 1 -->
+							<section class="container box feature1">
+								<div class="row">
+									<div class="12u">
+										<header class="first major">
+											<h2>Introducing your home and a few projects to pique your interest</h2>
+											<p>And this is where we talk about how <strong>awesome</strong> your week is going to be</p>
+										</header>
+									</div>
+								</div>
+								<div class="row">
+									<div class="4u">
+										<section>
+											<a href="#" class="image featured"><img id = "picHome" src="b.jpg" alt="Your home ( A picture of the college itself)" /></a>
+											<header class="second icon fa-user">
+												<h3>Your home for the next two years</h3>
+												
+											</header>
+										</section>
+									</div>
+									<div class="4u">
+										<section>
+											<a href="#" class="image featured"><img src="http://www.tembusucsc.com/images/slide4.jpg" alt="" /></a>
+											<header class="second icon fa-cog">
+												<h3>Create Your Memories</h3>
+											</header>
+										</section>
+									</div>
+									<div class="4u">
+										<section>
+											<a href="#" class="image featured"><img src="http://www.tembusucsc.com/images/slide1.jpg" alt="" /></a>
+											<header class="second icon fa-bar-chart-o">
+												<h3>Endless opportunities</h3>
+											
+											</header>
+										</section>
+									</div>
+									<div class="4u">
+										<section>
+											<a href="#" class="image featured"><img src="https://fbcdn-sphotos-a-a.akamaihd.net/hphotos-ak-xfp1/v/t1.0-9/11014903_10205326733140743_4684156739320663528_n.jpg?oh=d1e3696ced464a494e48c38c156c8e7e&oe=55A4E26C&__gda__=1438095113_94ac29e1fdb70a298da773b9aa678563" alt="" /></a>
+											<header class="second icon fa-bar-chart-o">
+												<h3>Environmental conservation</h3>
+											
+											</header>
+										</section>
+									</div>
+								
+								
+								
+					</div>
+				</div>
+			
+			
+
+		<!-- Footer Wrapper -->
+			<div id="footer-wrapper">
+				<footer id="footer" class="container">
+					<div class="row">
+						<div class="3u">
+						
+							<!-- Links -->
+								<section>
+									<h2>Associated websites
+								</h2>
+									<ul class="divided">
+										<li><a href="http://tembusu.nus.edu.sg"> Tembusu College</a></li>
+										<li><a href="http://www.tembusucsc.com">Tembusu College Student Committee</a></li>
+										<li><a href = "login.html">OGL Login</a></li>
+										
+									</ul>
+								</section>
+						
+						</div>
+						
+						<div class="6u">
+						
+							
+							
+						
+						</div>
+					</div>
+					<div class="row">
+
+						<div class="12u">
+							<div id="copyright">
+								<ul class="menu">
+									<li>&copy; Moazzam Khan || David Vasquez || Jerry. <br>All rights reserved</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</footer>
+			</div>
+
+	</body>
+</html>
